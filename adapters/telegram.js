f51563e9f23449ca51c2d9d4ea41b93f4653bac9
Rufus1123 <altru11235@gmail.com>
2020-01-18 02:43:17 +0100

    switching to different telegram api to parse commands that start with anything other than '/'

--- adapters/telegram.js
+++ adapters/telegram.js
@@ -1,16 +1,16 @@
 'use strict';
 
 require("dotenv").config();
-const TelegramClient = require('node-telegram-bot-api');
+const TelegramClient = require('telegram-bot-api');
 const Emulator = require('../gameboy/gameboy.js').Emulator;
+const fs = require('fs');
 var emulator = new Emulator("roms/FireRed.gba");
 
-const client = new TelegramClient(process.env.TelegramToken, {polling: true});
+const client = new TelegramClient({token: process.env.TelegramToken, updates: {enabled: true}});
 var lastMesageTimestamp = new Date().getTime();
 
 exports.init = function (){
   startMainLoop();
-  client.onText(/\//, (message) => onMessageReceived(message));
 }
 
 var startMainLoop = () => {
@@ -24,7 +24,9 @@ var startMainLoop = () => {
   }, 2000);
 };
 
-client.on('callback_query', (msg) => {
+client.on('message', (message) => onMessageReceived(message));
+
+client.on('inline.callback.query', (msg) => {
   msg.text = msg.data;
   msg.date = new Date().getTime();
   onMessageReceived(msg)
@@ -35,8 +37,8 @@ var onMessageReceived = (message) => {
       return;
   }
 
-  // Commands should start with a '/'
-  if (message.text.startsWith('/')){
+  // Commands should start with a '!'
+  if (message.text && message.text.startsWith('!')){
       lastMesageTimestamp = message.date * 1000;
       var command = message.text.slice(1).trim().toLowerCase();
 
@@ -69,7 +71,10 @@ function processCommand(command, message){
 function sendMessage(feedback) {
   var chatId = getGameboyChatId();
   if (feedback) {
-      client.sendMessage(chatId, feedback);
+      client.sendMessage({
+        chat_id: chatId, 
+        text: feedback
+      });
   }
 }
 
@@ -78,7 +83,7 @@ function processLoad(commands){
   if (state >= 0 && state < 6){
       emulator.readSaveFileAndReset(state);
   } else {
-      return "Specify a slot to load to, i.e. `/load 0` to load in slot 0.";
+      return "Specify a slot to load to, i.e. `!load 0` to load in slot 0.";
   }
 }
 
@@ -89,7 +94,7 @@ function processSave(commands, message){
       emulator.writeSaveFile(slot);
       return `Game has been saved into slot ${slot}.`;
   } else {
-      return "Specify a slot to save to, i.e. `/save 0` to save in slot 0.";
+      return "Specify a slot to save to, i.e. `!save 0` to save in slot 0.";
   }
 }
 
@@ -107,39 +112,43 @@ async function sendScreenshot(chatId, file){
   var reply_markup = {
     inline_keyboard: [
       [
-        {text: "A", callback_data: "/A"},
-        {text: "↑", callback_data: "/UP"},
-        {text: "B", callback_data: "/B"}
+        {text: "A", callback_data: "!A"},
+        {text: "↑", callback_data: "!UP"},
+        {text: "B", callback_data: "!B"}
       ],[
-        {text: "←", callback_data: "/LEFT"},
-        {text: "↓", callback_data: "/DOWN"},
-        {text: "→", callback_data: "/RIGHT"}
+        {text: "←", callback_data: "!LEFT"},
+        {text: "↓", callback_data: "!DOWN"},
+        {text: "→", callback_data: "!RIGHT"}
       ],[
-        {text: "START", callback_data: "/START"},
-        {text: "SELECT", callback_data: "/SELECT"}
+        {text: "START", callback_data: "!START"},
+        {text: "SELECT", callback_data: "!SELECT"}
       ]
     ]
   }
 
-  await client.sendPhoto(chatId, file, {
-    "reply_markup": reply_markup
+  fs.writeFileSync("screenshots/screenshot.png", file)
+
+  await client.sendPhoto({
+    chat_id: chatId, 
+    photo: "screenshots/screenshot.png",
+    reply_markup: JSON.stringify(reply_markup)
   });
 }
 
 function sendHelpMessage(){
-  var helpText = "Start your input with a `/`. The emulator reacts to " +
+  var helpText = "Start your input with a `!`. The emulator reacts to " +
                  "`up`, `down`, `left`, `right`, `a`, `b`, `start` and `select`.\n" +
-                 "Chaining commands: you can input multiple instructions at once, e.g. `/a down a`." +
+                 "Chaining commands: you can input multiple instructions at once, e.g. `!a down a`." +
                  "Each button will be pressed for 300ms. A dot `.` corresponds to not pressing a button\n" +
                  "Holding buttons: prefix an underscore `_` to hold a button for the duration of a chained" +
-                 "input, e.g. `/_b up5s` holds the `B` button down while pressing `↑`.\n" +
-                 "Advanced commands: postfix a duration to hold the button, e.g. `/b500ms` " +
-                 "to hold `B` for 500 milliseconds or `/right2s` to hold `→` for 2 seconds.\n" +
-                 "Saving and Loading: `/save n` will save the current save on the Gameboys Memory Card to " +
-                 "slot `n`. `/load n` will load the save into the Gameboys Memory Card and restart the " +
+                 "input, e.g. `!_b up5s` holds the `B` button down while pressing `↑`.\n" +
+                 "Advanced commands: postfix a duration to hold the button, e.g. `!b500ms` " +
+                 "to hold `B` for 500 milliseconds or `!right2s` to hold `→` for 2 seconds.\n" +
+                 "Saving and Loading: `!save n` will save the current save on the Gameboys Memory Card to " +
+                 "slot `n`. `!load n` will load the save into the Gameboys Memory Card and restart the " +
                  "Game so you can load the specified save file. Slots 0, 1 and 2 can be used by everyone. " +
                  "Slots 3, 4 and 5 are reserved for moderators.\n" +
-                 "Note: After saving the game, use `/save n` to create a save state in slot `n`."
+                 "Note: After saving the game, use `!save n` to create a save state in slot `n`."
   
   return helpText;
 }
